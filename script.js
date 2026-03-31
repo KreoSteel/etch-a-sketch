b20371c61982710dc95a8b30fa6a2f41b615960b
KreoSteel <tatarchukluca@gmail.com>
2026-03-31 18:54:28 +0200

    Add final features, rainbow colors mode, color picker, and clear grid button

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let gridSize = 16;
 
+// Selected elements
 const gridContainer = document.querySelector("#grid-container");
+
 const createGridBtn = document.querySelector("#create-grid-btn");
+const clearGridBtn = document.querySelector("#clear-grid-btn");
+
 const rainbowBtn = document.querySelector("#rainbow-btn");
+const colorPicker = document.querySelector("#color-picker");
+let color = colorPicker.value;
 
-createGridBtn.addEventListener("click", () => {
-  const response = prompt("Select the grid size (1, 100)");
-
-  if (response < 100 || response > 2) {
-    gridSize = response;
-    createGrid(gridSize);
-  } else {
-    alert("Limits of the grid is 2 from 100");
-  }
-});
-
-function createGrid(size) {
+// Functions
+function createGrid(size, isRainbowMode = false) {
   gridContainer.replaceChildren();
   let totalGrid = size * size; //default value: 16x16 = 256
 
@@ -25,12 +21,39 @@ function createGrid(size) {
     const gridCell = document.createElement("div");
     gridContainer.append(gridCell);
     gridCell.addEventListener("mouseover", () => {
-      gridCell.style = "background: #3D3B30";
+      gridCell.style = `background: ${color}`;
     });
+    if (isRainbowMode) {
+      gridCell.addEventListener("mouseover", () => {
+        rainbowMode(gridCell);
+      });
+    }
   }
-  
-  console.log(totalGrid);
-  
 }
+
+function rainbowMode(gridCell) {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  gridCell.style = `background: rgb(${red}, ${green}, ${blue})`;
+}
+
+// Event Listeners
+colorPicker.addEventListener("input", (e) => {
+  color = e.target.value;
+});
+rainbowBtn.addEventListener("click", () => createGrid(gridSize, true));
+clearGridBtn.addEventListener("click", () => createGrid(gridSize));
+
+createGridBtn.addEventListener("click", () => {
+  const response = prompt("Select the grid size (2, 100)");
+  if (response > 100 || response < 2) {
+    alert("Limit of the grid is from 2 to 100");
+  } else {
+    gridSize = response;
+    createGrid(gridSize);
+  }
+});
 
 createGrid(gridSize);
